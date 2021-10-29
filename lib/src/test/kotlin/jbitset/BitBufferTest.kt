2be8e8fe6b2b2f13a2ecf47a3ff121e116f60004
Nabarun Mondal <nabarun.mondal@gmail.com>
2021-10-30 01:31:28 +0530

    Intersection is up.

--- lib/src/test/kotlin/jbitset/BitBufferTest.kt
+++ lib/src/test/kotlin/jbitset/BitBufferTest.kt
@@ -10,13 +10,11 @@ class BitBufferTest {
     private val random = SecureRandom()
     lateinit var bb1: BitBuffer
     lateinit var bb2: BitBuffer
-    lateinit var bb3: BitBuffer
 
     @BeforeTest
     fun setup() {
         bb1 = BitBuffer()
         bb2 = BitBuffer()
-        bb3 = BitBuffer()
     }
 
     @Test
@@ -86,6 +84,56 @@ class BitBufferTest {
         (0 until bbu.size).forEach {
             val shouldNotBeBitValue = skippedBits.contains(it)
             Assert.assertNotEquals(shouldNotBeBitValue, bbu[it] )
+        }
+    }
+
+    @Test
+    fun testIntersection(){
+        // case 1 same set
+        bb1[0] = true
+        bb1[42] = true
+        bb2[0] = true
+        bb2[42] = true
+        var bbi = bb1.intersection(bb2)
+        Assert.assertEquals(bb1, bbi)
+        Assert.assertEquals(bb2, bbi)
+        // case 2 one is super set of another
+        bb2[40] = true
+        bbi = bb1.intersection(bb2)
+        Assert.assertEquals(bb1, bbi)
+        Assert.assertNotEquals(bb2, bbi)
+        // case 3 completely disjoint
+        (0 until bb1.size).forEach {
+            val bitValue = random.nextBoolean()
+            bb1[it] = bitValue
+            bb2[it] = !bitValue
+        }
+        bbi = bb1.intersection(bb2)
+        // this should be all zero, by definition
+        (0 until bbi.size).forEach {
+            Assert.assertFalse( bbi[it])
+        }
+        // now specific case of intersection only
+        val commonBits = (0 until bb1.size).filter {
+            bb1[it] = true
+            bb2[it] = true
+            var common = false
+            when ( random.nextInt(4) ){
+                0 -> bb2[it] = false
+                1 -> bb1[it] = false
+                2 -> {
+                    bb1[it] = false
+                    bb2[it] = false
+                }
+                3 -> common = true
+            }
+            common
+        }.toSet()
+        bbi = bb1.intersection(bb2)
+        // this guy must have all bits set apart from skipped bits , yes?
+        (0 until bbi.size).forEach {
+            val shouldBeThere = commonBits.contains(it)
+            Assert.assertEquals(shouldBeThere, bbi[it] )
         }
     }
 }
